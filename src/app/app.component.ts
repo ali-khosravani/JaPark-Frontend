ff652535de7d29ec107c02f92275a492b37bcf26
Ali Khosravani <129058763+ali-khosravani@users.noreply.github.com>
2025-06-11 17:26:42 +0330

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CopyrightFooterComponent } from "./layout/ui/footer/copyright-footer/copyright-footer.component";
 
 @Component({
   selector: 'ja-park-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CopyrightFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layout/ui/footer/copyright-footer/copyright-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ja-park-copyright-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './copyright-footer.component.html',
+  styleUrl: './copyright-footer.component.css'
+})
+export class CopyrightFooterComponent {
+
+}
